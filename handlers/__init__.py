d73e9cddb9fc6b60260598cf3e81da94e63057d2
Vitalii <vvkruhlyk@gmail.com>
2026-06-06 09:55:08 +0300

    chore: remove double import after rebase

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,5 @@
 from handlers.contact_handler import ContactHandler
 from handlers.decorators import input_error
-from handlers.task_handler import TaskHandler
 from handlers.note_handler import NoteHandler
 
 __all__ = ["ContactHandler", "NoteHandler", "input_error"]
