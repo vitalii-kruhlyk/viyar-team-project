69f30effcd0789b95fcf642c1274aeb5179d2ed4
Vyacheslav <Solyar92@gmail.com>
2026-06-07 12:11:00 +0300

    Update

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,6 +2,6 @@ from handlers.contact_handler import ContactHandler
 from handlers.decorators import input_error
 from handlers.note_handler import NoteHandler
 from handlers.task_handler import TaskHandler
-from handlers.api_hendler import APIHendler
+from handlers.api_handler import APIHendler
 
 __all__ = ["ContactHandler", "NoteHandler", "input_error", "TaskHandler", "APIHendler"]
