d4ae7cf0431a95c2285e00486db81062faf2bada
Vyacheslav <Solyar92@gmail.com>
2026-06-09 21:44:31 +0300

    add JsonFileHandler

--- storage/json_storage.py
+++ storage/json_storage.py
@@ -1,12 +1,18 @@
 import json
 from pathlib import Path
 
+from handlers.decorators import input_error
+
 
 class JsonStorage:
     path: Path
 
-    def __init__(self, filename: str) -> None:
-        self.path = Path(__file__).resolve().parent.parent / filename
+    def __init__(self, filename: str, file_dir: str = None) -> None:
+
+        if dir is None:
+            self.path = Path(__file__).resolve().parent.parent / filename
+        else:
+            self.path = Path(file_dir) / filename
 
     def load(self) -> list[dict]:
         if not self.path.exists():
@@ -25,3 +31,34 @@ class JsonStorage:
             json.dump(data, file, indent=4, ensure_ascii=False)
 
         temp_file.replace(self.path)
+
+class JsonFileHandler:
+    storage: JsonStorage
+
+    def __init__(self) -> None:
+        pass
+
+    @input_error
+    def import_file(self, flags: dict[str, str]) -> tuple[str, bool]:
+
+        if "-f" not in flags or "-path" not in flags:
+            raise ValueError("Usage: file --import-contacts -f <format> -path <file_path>")
+
+        file_format = flags["-f"]
+        file_path = flags["-path"]
+
+        return "", False
+
+    @input_error
+    def export_file(self, flags: dict[str, str]) -> tuple[str, bool]:
+
+        if "-f" not in flags or "-path" not in flags:
+            raise ValueError("Usage: file --export-contacts -f <format> -path <file_path>")
+
+        file_format = flags["-f"]
+        file_path = flags["-path"]
+
+
+
+
+        return "", False
